bb8a7a3b9120e7a0695bdbc0c0151a50afe85ee5
Mike Ireland <michael.ireland@anu.edu.au>
2016-03-14 00:18:21 +1100

    Now have the main AO elements (basic DM and wavefront sensor)

--- pyxao/deformable_mirror.py
+++ pyxao/deformable_mirror.py
@@ -2,10 +2,84 @@ from __future__ import division, print_function
 import opticstools as ot
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.ndimage as nd
 import pdb
 plt.ion()
 
 class DeformableMirror():
     """This is the deformable mirror class. """
-    def __init__(self,influence_function='gaussian'):
-        return
+    def __init__(self,influence_function='gaussian',wavefronts=[],central_actuator=False,\
+        plotit=False,actuator_pitch=0.5,geometry='hexagonal',edge_radius=1.4):
+        """ The geometry of this class has a lot in common with the ShackHartmann class
+        """
+        
+        if len(wavefronts)==0:
+            print("ERROR: Must initialise the DeformableMirror with a wavefront list")
+            raise UserWarning
+        
+        self.wavefronts = wavefronts
+        self.actuator_pitch = actuator_pitch
+        self.influence_function=influence_function
+        
+        #Create actuator geometry
+        xpx = []
+        ypx = []
+        lw = actuator_pitch/wavefronts[0].m_per_pix
+        nactuators = int(np.floor(wavefronts[0].sz/lw))
+        if geometry == 'hexagonal':
+             nrows = np.floor(nactuators / np.sqrt(3) )
+             xpx = np.tile(wavefronts[0].sz//2 + (np.arange(nactuators) - nactuators//2)*lw,nrows)
+             xpx = np.append(xpx,np.tile(wavefronts[0].sz//2 - lw/2 + (np.arange(nactuators) - nactuators//2)*lw,nrows-1))
+             ypx = np.repeat( wavefronts[0].sz//2 + (np.arange(nrows) - nrows//2)*np.sqrt(3)*lw,nactuators)
+             ypx = np.append(ypx,np.repeat( wavefronts[0].sz//2 -np.sqrt(3)/2*lw + (np.arange(nrows-1) - nrows//2+1)*np.sqrt(3)*lw,nactuators))
+             if not central_actuator:
+                xpx += lw/2
+                ypx += lw*np.sqrt(3)/4
+                
+        elif geometry == 'square':
+            xpx = np.tile(   wavefronts[0].sz//2 + (np.arange(nactuators) - nactuators//2)*lw,nactuators)
+            ypx = np.repeat( wavefronts[0].sz//2 + (np.arange(nactuators) - nactuators//2)*lw,nactuators)
+            if not central_actuator:
+                xpx += lw/2
+                ypx += lw/2
+        else:
+            print("ERROR: invalid actuator geometry")
+            raise UserWarning    
+        #Find actuators within half an actuator pitch of the pupil only
+        px = np.array( [xpx,ypx]).T
+        acirc = ot.utils.circle(wavefronts[0].sz,lw*edge_radius)
+        good=[]
+        for p in px:
+            overlap = np.sum(wavefronts[0].pupil*nd.interpolation.shift(acirc,\
+                (p[1]-wavefronts[0].sz//2,p[0]-wavefronts[0].sz//2),order=1))
+            good.append(np.sum(overlap)>0)
+        good = np.array(good)
+        #pdb.set_trace()
+        px = px[good]
+        self.px=px
+        if plotit:
+            plt.clf()
+            plt.plot(px[:,0], px[:,1],'o')
+        self.nactuators = px.shape[0]
+        
+    def apply(self,actuators):
+        """Go through all wavefronts and apply this DM"""
+        if len(actuators) != len(self.px):
+            print("ERROR: Wrong number of actuator values - should be {0:d}".format(len(self.px)))
+            raise UserWarning
+        sz = self.wavefronts[0].sz
+        xx = np.arange(sz) - sz//2
+        xy = np.meshgrid(xx,xx)
+        rr = np.sqrt(xy[0]**2 + xy[1]**2)
+        aw = self.actuator_pitch/self.wavefronts[0].m_per_pix
+        if self.influence_function == 'gaussian':
+            gg = np.exp(-rr**2/2.0/(aw/2.3548)**2)
+        else:
+            print("ERROR: Undefined influence function")
+            raise UserWarning
+        phasescreen = np.zeros( (sz,sz) )
+        for i in range(len(self.px)):
+            phasescreen += nd.interpolation.shift(gg,(self.px[i,1]-sz//2,self.px[i,0]-sz//2),order=1)*actuators[i]
+        for wf in self.wavefronts:
+            wf.field = wf.field*np.exp(2j*np.pi*phasescreen/wf.wave)
+        
--- pyxao/wfs.py
+++ pyxao/wfs.py
@@ -3,6 +3,8 @@ import opticstools as ot
 import numpy as np
 import matplotlib.pyplot as plt
 import pdb
+import scipy.ndimage as nd
+plt.ion()
 
 class WFS():
     """This is a base wavefront sensor class. It is the most abstract perfect wavefront
@@ -16,6 +18,142 @@ class WFS():
             return np.angle(wavefront[pix_to_use])*self.waves[0]/2/np.pi
         else:
             return np.angle(wavefront)*self.waves[0]/2/np.pi
+
+class ShackHartmann(WFS):
+    """A Shack-Hartmann wavefront sensor"""
+    def __init__(self,mask=None,geometry='hexagonal',wavefronts=[],central_lenslet=True,
+        lenslet_pitch=0.5,sampling=1.0,plotit=False,weights=None):
+        """Initialise the wavefront sensor.
+        
+        All lenslets that have their center within the pupil are included.
+        
+        Parameters
+        ----------
+        sampling: float
+            wavefront sensor sampling as a multiple of nyquist
+        weights: float array
+            optional weighting of wavelengths.
+        """
+        if len(wavefronts)==0:
+            print("ERROR: Must initialise the ShackHartmann with a wavefront list")
+            raise UserWarning
+        
+        self.lenslet_pitch=lenslet_pitch
+        
+        #Create lenslet geometry
+        xpx = []
+        ypx = []
+        lw = lenslet_pitch/wavefronts[0].m_per_pix
+        nlenslets = int(np.floor(wavefronts[0].sz/lw))
+        if geometry == 'hexagonal':
+             nrows = np.floor(nlenslets / np.sqrt(3) )
+             xpx = np.tile(wavefronts[0].sz//2 + (np.arange(nlenslets) - nlenslets//2)*lw,nrows)
+             xpx = np.append(xpx,np.tile(wavefronts[0].sz//2 - lw/2 + (np.arange(nlenslets) - nlenslets//2)*lw,nrows-1))
+             ypx = np.repeat( wavefronts[0].sz//2 + (np.arange(nrows) - nrows//2)*np.sqrt(3)*lw,nlenslets)
+             ypx = np.append(ypx,np.repeat( wavefronts[0].sz//2 -np.sqrt(3)/2*lw + (np.arange(nrows-1) - nrows//2+1)*np.sqrt(3)*lw,nlenslets))
+             if not central_lenslet:
+                xpx += lw/2
+                ypx += lw*np.sqrt(3)/4
+                
+        elif geometry == 'square':
+            xpx = np.tile(   wavefronts[0].sz//2 + (np.arange(nlenslets) - nlenslets//2)*lw,nlenslets)
+            ypx = np.repeat( wavefronts[0].sz//2 + (np.arange(nlenslets) - nlenslets//2)*lw,nlenslets)
+            if not central_lenslet:
+                xpx += lw/2
+                ypx += lw/2
+        else:
+            print("ERROR: invalid wavefront sensor geometry")
+            raise UserWarning    
+        #Find pixel values in pupil only.
+        px = np.array( [xpx,ypx]).T
+        good = np.array([wavefronts[0].pupil[int(np.round(p[1])),int(np.round(p[0]))] != 0 for p in px])
+        px = px[good]
+        self.px=px
+        if plotit:
+            #plt.clf()
+            plt.plot(px[:,0], px[:,1],'o')
+        
+        #Now go through the wavefronts (i.e. wavelengths) and create the pupil functions
+        #and propagators. We first have to find the shortest wavelength (longest focal length)
+        self.wavefronts = wavefronts
+        if not weights:
+            self.weights = np.ones(len(self.wavefronts))
+        else:
+            self.weights = weights
+        self.waves = []
+        self.propagator_ixs = []
+        self.nlenslets = px.shape[0]
+        flength_trial = []
+        for wf in wavefronts:
+            #Compute focal length.
+            fratio = 2*wf.m_per_pix/wf.wave
+            flength_trial.append(fratio*lenslet_pitch)
+        flength = np.max(flength_trial)
+        self.flength=flength
+        self.pupils=[]
+        for wf in wavefronts:
+            self.waves.append(wf.wave)
+            self.propagator_ixs.append(len(wf.propagators))
+            wf.add_propagator(flength)
+            #Now the tricky bit... make lots of hexagons!
+            pupil = np.zeros( (wf.sz,wf.sz),dtype=np.complex)
+            one_lenslet = ot.curved_wf(wf.sz,wf.m_per_pix,f_length=flength,wave=wf.wave)
+            if geometry == 'hexagonal':
+                one_lenslet *= ot.utils.hexagon(wf.sz,lw)
+            elif geometry == 'square':
+                one_lenslet *= ot.utils.hexagon(wf.sz,lw)
+            else:
+                print("ERROR: invalid wavefront sensor geometry")
+                raise UserWarning
+            
+            for i in range(self.nlenslets):
+                #Shift doens't work on complex arrays, so we have to split into real and
+                #imaginary parts.
+                to_add = nd.interpolation.shift(one_lenslet.real,(px[i,1]-wf.sz//2,px[i,0]-wf.sz//2),order=1) + \
+                    1j * nd.interpolation.shift(one_lenslet.imag,(px[i,1]-wf.sz//2,px[i,0]-wf.sz//2),order=1)
+                pupil += to_add
+            #The interpolation above decreases amplitude: this restores the amplitude
+            nonzero = np.abs(pupil) > 0
+            pupil[nonzero] /= np.abs(pupil)[nonzero]
+            self.pupils.append(pupil)
+            
+    def sense(self,mode='gauss_weighted',window_hw=5, window_fwhm=7.0):
+        """Sense the tilt and flux modes.
+        
+        Parameters
+        ----------
+        mode: string
+            NOT IMPLEMENTED YET
+        """
+        sz = self.wavefronts[0].sz
+        self.im = np.zeros( (sz,sz) )
+        for i in range(len(self.wavefronts)):
+            self.wavefronts[i].field = self.wavefronts[i].field*self.pupils[i]
+            self.wavefronts[i].propagate(self.propagator_ixs[i])
+            self.im += self.weights[i] + np.abs(self.wavefronts[i].field)**2
+        
+        #Now sense the centroids.
+        xx = np.arange(2*window_hw + 1) - window_hw
+        xy = np.meshgrid(xx,xx)
+        xyf = np.empty( (3,self.nlenslets) )
+        for i in range(self.nlenslets):
+            x_int  = int(np.round(self.px[i][0]))
+            x_frac = self.px[i][0] - x_int
+            y_int  = int(np.round(self.px[i][1]))
+            y_frac = self.px[i][1] - y_int
+            subim = self.im[y_int-window_hw:y_int+window_hw+1,x_int-window_hw:x_int+window_hw+1]
+            gg = np.exp(-((xy[0] - x_frac)**2 + (xy[1] - y_frac)**2)/2.0/window_fwhm*2.3548)
+            xyf[2,i] = np.sum(subim*gg)
+            xyf[0,i] = np.sum((xy[0]-x_frac)*subim*gg)/xyf[2,i]
+            xyf[1,i] = np.sum((xy[1]-y_frac)*subim*gg)/xyf[2,i]
+        
+        #The flux variable only has any meaning with respect to the mean. 
+        #A logarithm should also be taken so that differences to a perfect image
+        #are meaningful.
+        xyf[2] /= np.mean(xyf[2])
+        xyf[2] = np.log(xyf[2])
+        
+        return xyf
 
 class APNLWFS(WFS):
     """The Asymmetric Pupil Non-Linear Wavefront Sensor"""
