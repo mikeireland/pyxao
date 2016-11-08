2fa09570a7703c11d4d07b3371263685ab8f92ce
Anna Zovaro <azovaro@Annas-dator.local>
2016-11-08 16:53:33 +1100

    Some code to test new branch features

--- wfs_test.py
+++ wfs_test.py
@@ -6,24 +6,43 @@ import numpy as np
 import ipdb
 
 wave_height_px = 256
-m_per_px = 0.01
 
 # Wavefronts
 pupil={'type':"annulus", 'dout':0.7,'din':0.32}
-wf = wavefront.Wavefront(wave=850e-9,m_per_px=m_per_px,pupil=pupil,sz=wave_height_px)
+wf = wavefront.Wavefront(wave=850e-9,
+	m_per_px=0.7/256,
+	pupil=pupil,
+	sz=wave_height_px)
+
+# wf_old = wavefront.Wavefront(wave=850e-9,
+# 	m_per_px=0.7/105,
+# 	pupil=pupil,
+# 	sz=105)
 
 # Atmosphere. Keep default values for now
-atm = atmosphere.Atmosphere(m_per_px=m_per_px)
+atm = atmosphere.Atmosphere(m_per_px=0.7/256)
+atm_old = atmosphere.Atmosphere(m_per_px=0.7/105)
 
 # Wavefront sensor. Keep it square for now
+# wfs_old = wfs.OldShackHartmann(
+# 	wavefronts=[wf],
+# 	lenslet_pitch_m = 0.7/7,
+# 	N_os = 1,
+# 	geometry = 'square',
+# 	central_lenslet=True,
+# 	plotit=True
+# 	)
+
 wfs = wfs.ShackHartmann(
 	wavefronts=[wf],
 	plate_scale_as_px = 3600 * np.rad2deg(850e-9/pupil['dout']/2),
 	detector_sz = 105,
-	nlenslets = 7,
+	nlenslets_per_side=7,
 	geometry = 'square',
 	plotit=True
 	)
+
+
 # In its current state, it seems that the number of pixels per lenslet is set by the wavefront's resolution. 
 # We should be able to set it independently instead!
 # Input arguments:
@@ -37,11 +56,14 @@ wfs = wfs.ShackHartmann(
 #	plate_scale_as_px = lambda / 2 / D
 # The pupil mask is calculated using the focal length, so 
 # we need to be able to convert N_os into focal length
-# Need to make sure it's not possible to input conflicting parameters - for now stick with N_os, detector size and # lenslets 
+# Need to make sure it's not possible to input conflicting parameters - 
+# for now stick with N_os, detector size and # lenslets 
 
-# OK - given N_os, detector_sz and nlenslets, how do we calculate the focal length/F#?
+# OK - given N_os, detector_sz and nlenslets, how do we calculate the focal 
+# length/F#?
 # plate_scale_as_px from N_os at the given wavelength
-# (note: will have to specify Nyquist sampling AT a wavelength! so might be better to put in plate scale instead)
+# (note: will have to specify Nyquist sampling AT a wavelength! so might be 
+# better to put in plate scale instead)
 # plate_scale_as_px = lambda / 2 / D / N_os, where
 # D = pupil['dout'] / nlenslets 
 # Ok, so how can we compute the fratio from this?
@@ -50,10 +72,8 @@ wfs = wfs.ShackHartmann(
 # where l_px = pupil['dout']/detector_sz
 # So fratio = 
 
-# Note: all of these dimensions have been scaled up to the size of the primary aperture.
-l_px_m = pupil['dout'] / detector_sz
-plate_scale_as_mm = plate_scale_as_px / l_px_m
-flength_m = 206265 / plate_scale_as_mm
+# Note: all of these dimensions have been scaled up to the size of the primary
+# aperture.
 
 # To test: initialise the WFS, flatten the field and sense!
 
